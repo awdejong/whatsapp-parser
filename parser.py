c3e70ef074ca383f76b7d52e0ab465aaeaa4a657
Alexander de Jong <awdejong@outlook.com>
2022-06-06 15:22:27 +0200

    Add missing return statement + formatting

--- parser.py
+++ parser.py
@@ -3,52 +3,51 @@ import re
 from datetime import datetime
 
 
-def whatsapp_parse(source_file, anonymize=True, exclude_media=True):
+def whatsapp_parse(source_file: str, anonymize: bool = True, exclude_media: bool = True) -> pd.DataFrame:
 
-    #Store individual lines from source file
+    # Store individual lines from source file
     with open(source_file, encoding="utf-8") as file:
         lines = file.readlines()
 
-    #Regex for "m/d/yy, hh:mm - " followed by a phone number or contact name and a ":"
-    rx = re.compile(r"((?:\d|1[0-2])\/(?:\d|[123]\d)\/(?:\d|\d\d)\, \d\d[:]\d\d) [-] (.+?)(?:[:] |left|added.+|changed the group.+)")
+    # Regex for "m/d/yy, hh:mm - " followed by a phone number or contact name and a ":"
+    rx = re.compile(
+        r"((?:\d|1[0-2])\/(?:\d|[123]\d)\/(?:\d|\d\d)\, \d\d[:]\d\d) [-] (.+?)(?:[:] |left|added.+|changed the group.+)")
 
     df = pd.DataFrame(columns=["timestamp", "user", "content"])
 
-    #Go through file line by line, add new row to dataframe for each message.
-    #When a line only has one element, it is a continuation of the previous message.
-
+    # Go through file line by line, add new row to dataframe for each message.
+    # When a line only has one element, it is a continuation of the previous message.
     for line in lines:
         parts = rx.split(line)
 
         if len(parts) > 1:
-            row =  pd.DataFrame([parts[1:]], columns=["timestamp", "user", "content"])
+            row = pd.DataFrame([parts[1:]], columns=[
+                               "timestamp", "user", "content"])
             df = pd.concat([df, row])
 
         else:
-            if len(df)>0:
+            if len(df) > 0:
                 last = len(df)-1
                 df.iloc[last]["content"] = df.iloc[last]["content"] + parts[0]
 
-    #Remove empty messages and messages that only contained an image originally
+    # Remove empty messages and messages that only contained an image originally
     if exclude_media:
-        df = df.drop(df[(df.content == "\n") | (df.content == "<Media omitted>\n")].index)
+        df = df.drop(df[(df.content == "\n") | (
+            df.content == "<Media omitted>\n")].index)
     else:
         df = df.drop(df[df.content == "\n"].index)
 
     df.reset_index(drop=True, inplace=True)
 
-    #Factorize phone numbers and contact names
-    #Results in every user having a unique, anonymous ID
+    # Factorize phone numbers and contact names
+    # Results in every user having a unique, anonymous ID
     if anonymize:
         df["user_id"] = pd.Series(pd.factorize(df.user)[0])
 
-    #Convert text timestamp to datetime format
-    df["datetime"] = df.timestamp.map(lambda x: datetime.strptime(x, "%m/%d/%y, %H:%M"))
+    # Convert text timestamp to datetime format
+    df["datetime"] = df.timestamp.map(
+        lambda x: datetime.strptime(x, "%m/%d/%y, %H:%M"))
 
     df = df[["datetime", "userid", "content"]]
 
-
-    
-
-    
-
+    return df
